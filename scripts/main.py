f92785e3c901af812fee498d0f420a28c707ef9d
unknown <kenghzou.yeoh@gmail.com>
2023-06-01 13:53:58 +0800

    changes struct

--- scripts/main.py
+++ scripts/main.py
@@ -10,7 +10,271 @@ import os
 import modules.scripts as scripts
 from modules import script_callbacks
 
-from . import util
+
+version = "1.6.4"
+
+def_headers = {'User-Agent': 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'}
+
+
+proxies = None
+
+
+# print for debugging
+def printD(msg):
+    print(f"Civitai Downloader: {msg}")
+
+
+def read_chunks(file, size=io.DEFAULT_BUFFER_SIZE):
+    """Yield pieces of data from a file-like object until EOF."""
+    while True:
+        chunk = file.read(size)
+        if not chunk:
+            break
+        yield chunk
+
+# Now, hashing use the same way as pip's source code.
+def gen_file_sha256(filname):
+    printD("Use Memory Optimized SHA256")
+    blocksize=1 << 20
+    h = hashlib.sha256()
+    length = 0
+    with open(os.path.realpath(filname), 'rb') as f:
+        for block in read_chunks(f, size=blocksize):
+            length += len(block)
+            h.update(block)
+
+    hash_value =  h.hexdigest()
+    printD("sha256: " + hash_value)
+    printD("length: " + str(length))
+    return hash_value
+
+
+
+# get preview image
+def download_file(url, path):
+    printD("Downloading file from: " + url)
+    # get file
+    r = requests.get(url, stream=True, headers=def_headers, proxies=proxies)
+    if not r.ok:
+        printD("Get error code: " + str(r.status_code))
+        printD(r.text)
+        return
+    
+    # write to file
+    with open(os.path.realpath(path), 'wb') as f:
+        r.raw.decode_content = True
+        shutil.copyfileobj(r.raw, f)
+
+    printD("File downloaded to: " + path)
+
+# get subfolder list
+def get_subfolders(folder:str) -> list:
+    printD("Get subfolder for: " + folder)
+    if not folder:
+        printD("folder can not be None")
+        return
+    
+    if not os.path.isdir(folder):
+        printD("path is not a folder")
+        return
+    
+    prefix_len = len(folder)
+    subfolders = []
+    for root, dirs, files in os.walk(folder, followlinks=True):
+        for dir in dirs:
+            full_dir_path = os.path.join(root, dir)
+            # get subfolder path from it
+            subfolder = full_dir_path[prefix_len:]
+            subfolders.append(subfolder)
+
+    return subfolders
+
+
+# get relative path
+def get_relative_path(item_path:str, parent_path:str) -> str:
+    # printD("item_path:"+item_path)
+    # printD("parent_path:"+parent_path)
+    # item path must start with parent_path
+    if not item_path:
+        return ""
+    if not parent_path:
+        return ""
+    if not item_path.startswith(parent_path):
+        return item_path
+
+    relative = item_path[len(parent_path):]
+    if relative[:1] == "/" or relative[:1] == "\\":
+        relative = relative[1:]
+
+    # printD("relative:"+relative)
+    return relative
+    
+    
+    
+
+dl_ext = ".downloading"
+
+# disable ssl warning info
+requests.packages.urllib3.disable_warnings()
+
+# output is downloaded file path
+def dl(url, filepath):
+    util.printD("Start downloading from: " + url)
+    # get file_path
+    file_path = ""
+    if filepath:
+        file_path = filepath
+    else:
+        util.printD("folder is none")
+        return
+
+    # first request for header
+    rh = requests.get(url, stream=True, verify=False, headers=util.def_headers, proxies=util.proxies)
+    # get file size
+    total_size = 0
+    total_size = int(rh.headers['Content-Length'])
+    util.printD(f"File size: {total_size}")
+
+    # if file_path is empty, need to get file name from download url's header
+    if not file_path:
+        filename = ""
+        if "Content-Disposition" in rh.headers.keys():
+            cd = rh.headers["Content-Disposition"]
+            # Extract the filename from the header
+            # content of a CD: "attachment;filename=FileName.txt"
+            # in case "" is in CD filename's start and end, need to strip them out
+            filename = cd.split("=")[1].strip('"')
+            if not filename:
+                util.printD("Fail to get file name from Content-Disposition: " + cd)
+                return
+            
+        if not filename:
+            util.printD("Can not get file name from download url's header")
+            return
+        
+        # with folder and filename, now we have the full file path
+        file_path = os.path.join(folder, filename)
+
+
+    util.printD("Target file path: " + file_path)
+    base, ext = os.path.splitext(file_path)
+
+    # check if file is already exist
+    count = 2
+    new_base = base
+    while os.path.isfile(file_path):
+        util.printD("Target file already exist.")
+        # re-name
+        new_base = base + "_" + str(count)
+        file_path = new_base + ext
+        count += 1
+
+    # use a temp file for downloading
+    dl_file_path = new_base+dl_ext
+
+
+    util.printD(f"Downloading to temp file: {dl_file_path}")
+
+    # check if downloading file is exsited
+    downloaded_size = 0
+    if os.path.exists(dl_file_path):
+        downloaded_size = os.path.getsize(dl_file_path)
+
+    util.printD(f"Downloaded size: {downloaded_size}")
+
+    # create header range
+    headers = {'Range': 'bytes=%d-' % downloaded_size}
+    headers['User-Agent'] = util.def_headers['User-Agent']
+
+    # download with header
+    r = requests.get(url, stream=True, verify=False, headers=headers, proxies=util.proxies)
+
+    # write to file
+    with open(dl_file_path, "ab") as f:
+        for chunk in r.iter_content(chunk_size=1024):
+            if chunk:
+                downloaded_size += len(chunk)
+                f.write(chunk)
+                # force to write to disk
+                f.flush()
+
+                # progress
+                progress = int(50 * downloaded_size / total_size)
+                sys.stdout.reconfigure(encoding='utf-8')
+                sys.stdout.write("\r[%s%s] %d%%" % ('-' * progress, ' ' * (50 - progress), 100 * downloaded_size / total_size))
+                sys.stdout.flush()
+
+    print()
+
+    # rename file
+    os.rename(dl_file_path, file_path)
+    util.printD(f"File Downloaded to: {file_path}")
+    return file_path
+    
+def dl_model_new_version(msg, max_size_preview, skip_nsfw_preview):
+    util.printD("Start dl_model_new_version")
+
+    output = ""
+
+    result = msg_handler.parse_js_msg(msg)
+    if not result:
+        output = "Parsing js ms failed"
+        util.printD(output)
+        return output
+    
+    model_path = result["model_path"]
+    version_id = result["version_id"]
+    download_url = result["download_url"]
+
+    util.printD("model_path: " + model_path)
+    util.printD("version_id: " + str(version_id))
+    util.printD("download_url: " + download_url)
+
+    # check data
+    if not model_path:
+        output = "model_path is empty"
+        util.printD(output)
+        return output
+
+    if not version_id:
+        output = "version_id is empty"
+        util.printD(output)
+        return output
+    
+    if not download_url:
+        output = "download_url is empty"
+        util.printD(output)
+        return output
+
+    if not os.path.isfile(model_path):
+        output = "model_path is not a file: "+ model_path
+        util.printD(output)
+        return output
+
+    # get model folder from model path
+    model_folder = os.path.dirname(model_path)
+
+    # no need to check when downloading new version, since checking new version is already checked
+    # check if this model is already existed
+    # r = civitai.search_local_model_info_by_version_id(model_folder, version_id)
+    # if r:
+    #     output = "This model version is already existed"
+    #     util.printD(output)
+    #     return output
+
+    # download file
+    new_model_path = downloader.dl(download_url, model_folder, None, None)
+    if not new_model_path:
+        output = "Download failed, check console log for detail. Download url: " + download_url
+        util.printD(output)
+        return output
+
+
+
+    output = "Done. Model downloaded to: " + new_model_path
+    util.printD(output)
+    return output
+    
 available_extensions = {"items": []}
 
 sort_ordering = [
